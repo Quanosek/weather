5605ae34bb9409e758df4ea45427085c1713e49c
quanosek <kubaklalo@gmail.com>
2024-07-28 14:06:52 +0200

    navigator critical hotfix

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useRouter } from "next/navigation";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 
 const appid = process.env.NEXT_PUBLIC_WEATHER_API_KEY;
@@ -11,26 +11,28 @@ export default function HomePage() {
 
   const [error, setError] = useState<string>();
 
-  navigator.geolocation.getCurrentPosition(
-    async (position) => {
-      const { latitude: lat, longitude: lon } = position.coords;
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      async (position) => {
+        const { latitude: lat, longitude: lon } = position.coords;
 
-      try {
-        // https://openweathermap.org/current
-        const { data } = await axios.get(
-          "https://api.openweathermap.org/data/2.5/weather",
-          { params: { lat, lon, appid, units: "metric", lang: "pl" } }
-        );
-        return router.replace("/city/" + data.id);
-      } catch (err) {
-        return console.error((err as Error).message);
+        try {
+          // https://openweathermap.org/current
+          const { data } = await axios.get(
+            "https://api.openweathermap.org/data/2.5/weather",
+            { params: { lat, lon, appid, units: "metric", lang: "pl" } }
+          );
+          return router.replace("/city/" + data.id);
+        } catch (err) {
+          return console.error((err as Error).message);
+        }
+      },
+      (err) => {
+        setError("Nie przyznano uprawnień do lokalizacji.");
+        console.error(err.message);
       }
-    },
-    (err) => {
-      setError("Nie przyznano uprawnień do lokalizacji.");
-      console.error(err.message);
-    }
-  );
+    );
+  }, [router]);
 
   if (error) return <p className="error">{error}</p>;
 }
